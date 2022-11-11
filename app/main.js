80309c5746470891d56480f7da6e5507a17a67f1
Karinny Lemos <99560287+karinnylemosb@users.noreply.github.com>
2022-11-11 11:04:02 -0300

    Criando a lista de livros dinâmica com o forEach

--- app/main.js
+++ app/main.js
@@ -1,11 +1,40 @@
+//Array de livros:
 let livros = [];
+
+//Endpoint da API:
 const endPointDaApi =
   'https://guilhermeonrails.github.io/casadocodigo/livros.json';
 
 getBuscarLivrosdaApi();
 
+const elementoParaInserirLivros = document.getElementById('livros');
+
+//Função fetch:
 async function getBuscarLivrosdaApi() {
   const res = await fetch(endPointDaApi);
   livros = await res.json();
   console.table(livros);
+  exibirOsLivrosNaTela(livros);
+}
+
+function exibirOsLivrosNaTela(listaDeLivros) {
+  listaDeLivros.forEach((livro) => {
+    elementoParaInserirLivros.innerHTML += `
+    <div class="livro">
+      <img class="livro__imagens" src=${livro.imagem} alt=${livro.alt} />
+
+      <h2 class="livro__titulo">
+        ${livro.titulo}
+      </h2>
+
+      <p class="livro__descricao">${livro.autor}</p>
+
+      <p class="livro__preco" id="preco">${livro.preco}</p>
+
+      <div class="tags">
+        <span class="tag">${livro.categoria}</span>
+      </div>
+    </div>
+    `;
+  });
 }
