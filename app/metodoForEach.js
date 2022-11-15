72aca915aeb7cc55681e78e19e37f1a07d5ceb3e
Karinny Lemos <99560287+karinnylemosb@users.noreply.github.com>
2022-11-15 15:38:18 -0300

    Usando operador ternário

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -3,9 +3,12 @@ const elementoParaInserirLivros = document.getElementById('livros');
 function exibirOsLivrosNaTela(listaDeLivros) {
   elementoParaInserirLivros.innerHTML = '';
   listaDeLivros.forEach((livro) => {
+    //
+    let disponibilidade =
+      livro.quantidade > 0 ? 'livro__imagens' : 'livro__imagens indisponivel'; //operador ternário
     elementoParaInserirLivros.innerHTML += `
     <div class="livro">
-      <img class="livro__imagens" src=${livro.imagem} alt=${livro.alt} />
+      <img class="${disponibilidade}" src=${livro.imagem} alt=${livro.alt} />
 
       <h2 class="livro__titulo">
         ${livro.titulo}
@@ -22,6 +25,14 @@ function exibirOsLivrosNaTela(listaDeLivros) {
     `;
   });
 }
+
+// function verificarDisponibilidadeDoLivro(livro) {
+//   if (livro.quantidade > 0) {
+//     return 'livro_imagens';
+//   } else {
+//     return 'livro_imagens indisponivel';
+//   }
+// }
 
 //O .map percorre cada item do array (como o forEach) para modificar algo nesses itens. Ele não altera o array original, ele cria outra variável com os dados modificados. Ela sempre retorna um valor.
 //E essa é a principal diferença entre os métodos map e forEach. Sendo assim, o forEach manipula os dados reais de um array e o map cria um novo array.
